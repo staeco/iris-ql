e9ed6c5aeb0d289945b99dd771ac64afdf4a9792
contra <yo@contra.io>
2019-07-23 13:51:50 -0400

    local test setup fixes

--- test/fixtures/db.js
+++ test/fixtures/db.js
@@ -1,7 +1,7 @@
 import types from 'sequelize'
 import { connect } from '../../src'
 
-const db = connect(process.env.POSTGRES_URL || 'postgres://localhost/iris-ql')
+const db = connect(process.env.POSTGRES_URL || 'postgres://postgres@localhost/iris-ql')
 
 db.define('user', {
   id: {
