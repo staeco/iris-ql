1be868b29bc3529a7d1445efecfb0c59269627f4
contra <yo@contra.io>
2019-07-26 12:13:23 -0400

    grouping tests

---
+++ test/AnalyticsQuery/options/groupings.js
@@ -0,0 +1,90 @@
+import should from 'should'
+import { Connection, AnalyticsQuery } from '../../../src'
+import db from '../../fixtures/db'
+
+describe('AnalyticsQuery#options#groupings', () => {
+  const conn = new Connection(db)
+  const { user } = conn.tables()
+  it('should execute with a field', async () => {
+    const query = new AnalyticsQuery({
+      aggregations: [
+        {
+          value: { function: 'count' },
+          alias: 'count'
+        },
+        {
+          value: { field: 'name' },
+          alias: 'name'
+        }
+      ],
+      groupings: [
+        { field: 'name' }
+      ]
+    }, { table: user })
+    const res = await query.execute()
+    should.exist(res)
+    res.length.should.eql(3)
+    res[0].count.should.eql(1)
+  })
+  it('should return grouping invalid field errors correctly', async () => {
+    try {
+      new AnalyticsQuery({
+        aggregations: [
+          {
+            value: { function: 'count' },
+            alias: 'count'
+          },
+          {
+            value: { field: 'name' },
+            alias: 'name'
+          }
+        ],
+        groupings: [
+          { field: 'does-not-exist' }
+        ]
+      }, { table: user })
+    } catch (err) {
+      should.exist(err)
+      should.exist(err.fields)
+      err.fields.should.eql([ {
+        path: [ 'groupings', 0, 'field' ],
+        value: 'does-not-exist',
+        message: 'Field does not exist.'
+      } ])
+      return
+    }
+    throw new Error('Did not throw!')
+  })
+  it('should return deep grouping value errors correctly', async () => {
+    try {
+      new AnalyticsQuery({
+        aggregations: [
+          {
+            value: { function: 'count' },
+            alias: 'count'
+          },
+          {
+            value: { field: 'name' },
+            alias: 'name'
+          }
+        ],
+        groupings: [
+          {
+            function: 'area',
+            arguments: [ { field: 'yo' } ]
+          }
+        ]
+      }, { table: user })
+    } catch (err) {
+      should.exist(err)
+      should.exist(err.fields)
+      err.fields.should.eql([ {
+        path: [ 'groupings', 0, 'arguments', 0, 'field' ],
+        value: 'yo',
+        message: 'Field does not exist.'
+      } ])
+      return
+    }
+    throw new Error('Did not throw!')
+  })
+})
