afb0f5796e52a68c9537ff4ec4f4d7ef9bef1663
Sam McCollum <smccollum@mccollum.enterprises>
2019-09-10 17:20:14 -0700

    chore: add iffy tests

--- test/util/iffy/number.js
+++ test/util/iffy/number.js
@@ -1,1 +1,17 @@
-// TODO-TEST: Sam
+import should from 'should'
+import number from '../../../src/util/iffy/number'
+
+describe('util#iffy#number', () => {
+  it('should exist', () => {
+    should(number).not.be.null()
+  })
+  it('should process number', () => {
+    number(5).should.eql(5)
+  })
+  it('should process number string', () => {
+    number('256').should.eql(256)
+  })
+  it('should throw error on invalid number', () => {
+    should.throws(() => number('fity'))
+  })
+})
--- test/util/iffy/stringArray.js
+++ test/util/iffy/stringArray.js
@@ -1,1 +1,13 @@
-// TODO-TEST: Sam
+import should from 'should'
+import stringArray from '../../../src/util/iffy/stringArray'
+
+describe('util#iffy#stringArray', () => {
+  it('should exist', () => {
+    should(stringArray).not.be.null()
+  })
+  it('should create string array', () => {
+    const sample = [ 'a', 'b', 'c', 'd', 'e', 'f' ]
+    stringArray(sample).should.eql(sample)
+    stringArray(sample.join(',')).should.eql(sample)
+  })
+})
