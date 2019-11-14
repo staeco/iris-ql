058992820f0ae8dda97da6712601d1236360b82e
contra <yo@contra.io>
2019-11-14 12:18:56 -0500

    feat: optimize geojson parsing performance

--- src/sql/geospatial.sql
+++ src/sql/geospatial.sql
@@ -1,10 +1,21 @@
+CREATE OR REPLACE FUNCTION from_geojson(p_input text) RETURNS geometry AS $$
+  SELECT ST_MakeValid(ST_GeomFromGeoJSON(p_input));
+$$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE
+RETURNS NULL ON NULL INPUT;
+
+CREATE OR REPLACE FUNCTION from_geojson(p_input jsonb) RETURNS geometry AS $$
+  SELECT ST_MakeValid(ST_GeomFromGeoJSON(p_input));
+$$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE
+RETURNS NULL ON NULL INPUT;
+
 CREATE OR REPLACE FUNCTION geocollection_from_geojson(p_input text) RETURNS geometry AS $$
-  SELECT ST_Collect(ST_MakeValid(ST_GeomFromGeoJSON(feat->>'geometry')))
+  SELECT ST_Collect(from_geojson(feat->'geometry'))
   FROM (SELECT jsonb_array_elements(p_input::jsonb->'features') AS feat) AS f;
 $$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE
 RETURNS NULL ON NULL INPUT;
 
-CREATE OR REPLACE FUNCTION from_geojson(p_input text) RETURNS geometry AS $$
-  SELECT ST_MakeValid(ST_GeomFromGeoJSON(p_input));
+CREATE OR REPLACE FUNCTION geocollection_from_geojson(p_input jsonb) RETURNS geometry AS $$
+  SELECT ST_Collect(from_geojson(feat->'geometry'))
+  FROM (SELECT jsonb_array_elements(p_input->'features') AS feat) AS f;
 $$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE
 RETURNS NULL ON NULL INPUT;
