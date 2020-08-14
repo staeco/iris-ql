38f6d1cbb7f9175cbbee5f6f625faee421e8591d
contra <yo@contra.io>
2020-08-13 17:45:05 -0700

    chore: add test

--- test/Query/executeStream.js
+++ test/Query/executeStream.js
@@ -3,6 +3,8 @@ import types from 'sequelize'
 import { Query } from '../../src'
 import db from '../fixtures/db'
 import collect from 'get-stream'
+import pumpify from 'pumpify'
+import through2 from 'through2'
 import JSONStream from 'jsonstream-next'
 
 const json = () => JSONStream.stringify('[', ',', ']')
@@ -100,5 +102,21 @@ describe('Query#executeStream', () => {
     res.length.should.equal(1)
     should.not.exist(res[0].authToken)
     should.exist(res[0].name)
+  })
+  it('should work with format and pumpify', async () => {
+    const query = new Query({ limit: 1 }, { model: user.scope('public') })
+    const stream = await query.executeStream({
+      format: json
+    })
+    should(stream.contentType).eql(json.contentType)
+    const res = await collect(pumpify.obj(
+      stream,
+      through2()
+    ))
+    should(typeof res).eql('string')
+    const parsed = JSON.parse(res)
+    parsed.length.should.equal(1)
+    should.not.exist(parsed[0].authToken)
+    should.exist(parsed[0].name)
   })
 })
