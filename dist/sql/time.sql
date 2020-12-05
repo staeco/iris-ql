b68d5b9cd59fa076c696e126ca1489ba9dbc19f4
contra <yo@contra.io>
2020-12-04 22:20:37 -0700

    fix: type sig

--- dist/sql/time.sql
+++ dist/sql/time.sql
@@ -1,47 +1,49 @@
+-- date_part has a different type sig on 12 and 13 so just make it numeric everywhere
+-- on 13 its integer, 12 its double precision
 CREATE OR REPLACE FUNCTION force_tz(base_date timestamptz, tz text) RETURNS timestamp AS $$
-  SELECT base_date AT TIME ZONE tz
+  SELECT base_date AT TIME ZONE tz;
 $$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE
 RETURNS NULL ON NULL INPUT;
 
 CREATE OR REPLACE FUNCTION time_to_ms(a timestamptz) RETURNS numeric AS $$
-  SELECT (date_part('epoch', a) * 1000)::numeric
+  SELECT date_part('epoch', a)::numeric * 1000;
 $$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE
 RETURNS NULL ON NULL INPUT;
 
 CREATE OR REPLACE FUNCTION time_to_ms(a timestamp) RETURNS numeric AS $$
-  SELECT (date_part('epoch', a) * 1000)::numeric
+  SELECT date_part('epoch', a)::numeric * 1000;
 $$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE
 RETURNS NULL ON NULL INPUT;
 
 CREATE OR REPLACE FUNCTION parse_iso(a text) RETURNS timestamptz AS $$
-  SELECT to_timestamp(a, 'YYYY-MM-DD"T"HH24:MI:SS"Z"')
+  SELECT to_timestamp(a, 'YYYY-MM-DD"T"HH24:MI:SS"Z"');
 $$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE
 RETURNS NULL ON NULL INPUT;
 
 -- custom utils
 CREATE OR REPLACE FUNCTION get_custom_year(v timestamp, custom_year_start integer) RETURNS numeric AS $$
   SELECT CASE
-  	WHEN date_part('month', v) >= custom_year_start THEN date_part('year', v) + 1
-  	ELSE date_part('year', v)
+  	WHEN date_part('month', v) >= custom_year_start THEN date_part('year', v)::numeric + 1
+  	ELSE date_part('year', v)::numeric
   	END;
 $$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE
 RETURNS NULL ON NULL INPUT;
 
 CREATE OR REPLACE FUNCTION get_custom_year(v timestamptz, custom_year_start integer) RETURNS numeric AS $$
   SELECT CASE
-  	WHEN date_part('month', v) >= custom_year_start THEN date_part('year', v) + 1
-  	ELSE date_part('year', v)
+  	WHEN date_part('month', v) >= custom_year_start THEN date_part('year', v)::numeric + 1
+  	ELSE date_part('year', v)::numeric
   	END;
 $$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE
 RETURNS NULL ON NULL INPUT;
 
 CREATE OR REPLACE FUNCTION get_custom_quarter(v timestamp, custom_year_start integer) RETURNS numeric AS $$
-  SELECT floor(((12 + date_part('month', v)::integer - custom_year_start) % 12) / 3 ) + 1;
+  SELECT floor(((12 + date_part('month', v)::numeric - custom_year_start) % 12) / 3 ) + 1;
 $$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE
 RETURNS NULL ON NULL INPUT;
 
 CREATE OR REPLACE FUNCTION get_custom_quarter(v timestamptz, custom_year_start integer) RETURNS numeric AS $$
-  SELECT floor(((12 + date_part('month', v)::integer - custom_year_start) % 12) / 3 ) + 1;
+  SELECT floor(((12 + date_part('month', v)::numeric - custom_year_start) % 12) / 3 ) + 1;
 $$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE
 RETURNS NULL ON NULL INPUT;
 
@@ -52,12 +54,12 @@ need to invert this for some reason?
   return (12 + (v - 1) + custom_year_start) % 12
 */
 CREATE OR REPLACE FUNCTION get_custom_month(v timestamp, custom_year_start integer) RETURNS numeric AS $$
-  SELECT ((12 + date_part('month', v)::integer - custom_year_start) % 12) + 1;
+  SELECT ((12 + date_part('month', v)::numeric - custom_year_start) % 12) + 1;
 $$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE
 RETURNS NULL ON NULL INPUT;
 
 CREATE OR REPLACE FUNCTION get_custom_month(v timestamptz, custom_year_start integer) RETURNS numeric AS $$
-  SELECT ((12 + date_part('month', v)::integer - custom_year_start) % 12) + 1;
+  SELECT ((12 + date_part('month', v)::numeric - custom_year_start) % 12) + 1;
 $$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE
 RETURNS NULL ON NULL INPUT;
 
@@ -66,7 +68,7 @@ RETURNS NULL ON NULL INPUT;
 CREATE OR REPLACE FUNCTION date_part_with_custom(part text, v timestamp, custom_year_start integer) RETURNS numeric AS $$
 BEGIN
   IF custom_year_start = 1 THEN
-    RETURN date_part(replace(part, 'custom_', ''), v);
+    RETURN date_part(replace(part, 'custom_', ''), v)::numeric;
   END IF;
   IF part = 'custom_year' THEN
     RETURN get_custom_year(v, custom_year_start);
@@ -85,7 +87,7 @@ RETURNS NULL ON NULL INPUT;
 CREATE OR REPLACE FUNCTION date_part_with_custom(part text, v timestamptz, custom_year_start integer) RETURNS numeric AS $$
 BEGIN
   IF custom_year_start = 1 THEN
-    RETURN date_part(replace(part, 'custom_', ''), v);
+    RETURN date_part(replace(part, 'custom_', ''), v)::numeric;
   END IF;
   IF part = 'custom_year'
   THEN
